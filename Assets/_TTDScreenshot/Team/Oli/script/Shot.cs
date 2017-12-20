2e9c2ef3bb4b218a1307ae6e6d49546df78973b5
Technifutur <600@technifutur.be>
2017-12-20 10:15:44 +0100

    création du screenshot press button p

---
+++ Assets/_TTDScreenshot/Team/Oli/script/Shot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//public AudioClip soundScreenShot;
+public class Shot : MonoBehaviour
+{
+    void TakeScreenShot (){
+
+ //GetComponent<AudioSource> (). PlayOneShot (soundScreenShot);//si j'ai pas ne pas le mettre
+
+string DT = System.DateTime.Now.ToString (); //renomer le jpg string pas int
+DT = DT.Replace ("/" , "-");
+DT = DT.Replace (":", "-");
+        
+    ScreenCapture.CaptureScreenshot ("capture"+ DT + ".png");
+        
+
+    }
+
+    void Update (){
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeScreenShot();
+        }         }
+}
+

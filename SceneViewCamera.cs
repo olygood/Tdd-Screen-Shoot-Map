d57fc0278dff0bcf59513ca72db8544d6a66dc06
Technifutur <600@technifutur.be>
2017-12-20 10:54:46 +0100

    Camera related scripts

---
+++ SceneViewCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SceneViewCamera : EditorWindow
+{
+    [MenuItem("Window/SceneViewCamera")]
+    static void Init()
+    {
+        SceneViewCamera window = (SceneViewCamera)EditorWindow.GetWindow(typeof(SceneViewCamera));
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.TextField("SceneViewCameraAngle", "" + SceneView.lastActiveSceneView.rotation);
+        EditorGUILayout.TextField("SceneViewCameraPosition", "" + SceneView.lastActiveSceneView.camera.transform.position);
+
+    }
+    
+}
